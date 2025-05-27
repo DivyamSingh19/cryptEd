4804d041e469e55595c64951de575f3d235395d7
Divyam Singh <divyamsingh1918@gmail.com>
2025-05-28 01:23:21 +0530

    updates

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,10 @@
- import { Hero } from '@/components/landing/Landing'
-import Navbar from '@/components/navbar/Navbar'
 import React from 'react'
  
  const page = () => {
    return (
      <div className='absolute top-0 left-0 w-screen h-screen'>
         
-       <Hero/>
+      
         
      </div>
    )
